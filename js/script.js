1fe8e4d1e2b7b3d35325ee7d302b8dd94d3a2454
mukhriddin03 <imuhriddin700@gmail.com>
2025-03-27 13:07:03 +0500

    changed

--- js/script.js
+++ js/script.js
@@ -4,10 +4,46 @@ console.log(leftSidebarBtn);
 
 function closeSidebar() {
     document.querySelector(".sidebar").style.display = "none";
-    document.querySelector(".sidebar-btn-main").style.display = "inline-block";
+    document.querySelector(".sidebar-btn-main").style.display = "block";
+    document.getElementById("newChat").style.display = "inline-block";
 }
 
 function openSidebar() {
     document.querySelector(".sidebar").style.display = "inline-block";
     document.querySelector(".sidebar-btn-main").style.display = "none";
+    document.getElementById("newChat").style.display = "none";
 }
+
+
+let textarea = document.getElementById("userQuestion");
+console.log(textarea.ariaValueNow);
+let mainChattingSection = document.querySelector(".main-chatting-section");
+
+function sendQuestion() {
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".chat-container").style.top = "87%";
+
+    mainChattingSection.style.display = "inline-block";
+
+    let question = document.createElement("div");
+    question.id = "question";
+    question.className = "question";
+
+    let message = document.createElement("p");
+    message.textContent = textarea.value;
+    textarea.value = '';
+
+    question.appendChild(message);
+
+    mainChattingSection.appendChild(question);
+
+    question.style.marginLeft = `${690 - question.clientWidth}px`;
+}
+
+
+textarea.addEventListener("keypress", function(event) {
+    if(event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+        sendQuestion();
+    }
+})
